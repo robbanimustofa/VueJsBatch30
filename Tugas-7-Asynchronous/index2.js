218ea890eed10c1ad9769bb61757ff8015286f93
robbanimustofa <robbanimustofa@gmail.com>
2021-12-09 08:01:36 +0700

    latihan

--- Tugas-7-Asynchronous/index2.js
+++ Tugas-7-Asynchronous/index2.js
@@ -8,15 +8,46 @@ var books1 = [
 ];
 
 let waktu1 = 5000;
-const baca1 = (time, books1, i) => {
-  if (i < books1.length) {
-    readBooksPromise(time, books1[i], (sisa) => {
-      if (sisa > 0) {
-        i += 1;
-        baca(sisa, books1, i);
+// const baca1 = (time, books1, i) => {
+//   readBooksPromise(time, books1[i], () => {
+// if (time > 0) {
+//   i += 1;
+//   baca1(time, books1, i);
+// }
+//   });
+// };
+// baca1(waktu1, books1, 0);
+
+const createPost = (book) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      books1.push(book);
+      const error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject();
       }
-    });
-  }
+    }, 2000);
+  });
 };
 
-baca1(waktu1, books1, 0);
+const getPosts = (time, book, i) => {
+  console.log(book);
+  // readBooksPromise(time, book, () => {
+  //   if (time > 0) {
+  //     i += 1;
+  //     console.log(i);
+  //     getPosts(time, i);
+  //   }
+  // });
+  // setTimeout(() => {
+  //   books1.forEach((book) => {
+  //     // console.log(book);
+  //   });
+  // }, 1000);
+};
+// getPosts(5000, books1);
+createPost()
+  .then(getPosts(5000, books1, 0))
+  .catch((error) => console.log(error));
--- Tugas-7-Asynchronous/latihanCallback.js
+++ Tugas-7-Asynchronous/latihanCallback.js
@@ -61,3 +61,17 @@ getPosts();
 // });
 
 // readBooks(1000, createPost(books), getPosts());
+
+// const baca1 = (time, books1, i) => {
+//   if (i < books1.length) {
+//     readBooksPromise(time,books1[i])
+//     readBooksPromise(time, books1[i], (sisa) => {
+//       if (sisa > 0) {
+//         i += 1;
+//         baca(sisa, books1, i);
+//       }
+//     });
+//   }
+// };
+
+// baca1(waktu1, books1, 0);
